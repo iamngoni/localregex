e85eb9263a6c3d0c6e921797bd3b03820e6a1d16
iamngoni <imngonii@gmail.com>
2022-08-19 19:58:54 +0200

    removed all code

--- lib/enum.dart
+++
@@ -1,8 +0,0 @@
-abstract class Enum<T> {
-  final T value;
-  const Enum(this.value);
-
-  String toString() {
-    return "${runtimeType.toString()}. ${value == null ? 'null' : value.toString()}";
-  }
-}
--- lib/password_validation.dart
+++
@@ -1,2 +0,0 @@
-export 'password_validation/password_text_form_field.dart';
-export 'password_validation/password_validation_section.dart';
